597923e28cbbf8de367b41223c57d3140c144859
Tammibriggs <Godsfirstbriggs@gmail.com>
2022-07-03 18:36:20 +0100

    webiny client pagination

--- src/App.js
+++ src/App.js
@@ -1,27 +1,95 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Book from './Book';
 import { Allbooks } from './data';
+import { useQuery, gql } from '@apollo/client';
 
-function App() {
+const LIST_BOOKS = gql`
+  query GetBooks ($limit: Int, $after: String){
+    listBooks (limit: $limit, after: $after){
+      data {
+        id
+        authors
+        description
+        image
+        previewLink
+        publishDate
+        publisher
+        title
+        subtitle
+      }
+      meta {
+        cursor
+        hasMoreItems
+      }
+      error {
+        message
+        data
+      }
+    }
+  }
+`
 
+function App() { 
   const [books, setBooks] = useState(Allbooks)
+  const [cursors, setCursors] = useState([null])
+  const [page, setPage] = useState(0)
+
+  const {data, loading} = useQuery(LIST_BOOKS, {
+    variables: {
+      limit: 10,
+      after: cursors[page]
+    }
+  })
+
+  useEffect(() => {
+    const pointer = data?.listBooks.meta?.cursor
+    if(!loading && pointer !== cursors[page]){
+      setCursors([...cursors, pointer])
+    }
+  }, [data])
 
   return (
     <div className="app">
-       <div className='books'>
-          {books?.map((book) => (
-            <Book
-              key={book.id}
-              title={book.title}
-              image={book.image}
-              authors={book.authors}
-              publisher={book.publisher}
-              publishDate={book.publishedDate}
-              id={book.id}
-            />
-          ))}
+      {!loading && data?.listBooks.data
+        ? <div className='books'>
+            {data?.listBooks.data?.map((book) => (
+                <Book
+                  key={book.id}
+                  title={book.title}
+                  image={book.image}
+                  authors={book.authors}
+                  publisher={book.publisher}
+                  publishDate={book.publishedDate}
+                  id={book.id}
+                />
+              ))}
+          </div>   
+        : <>
+          {loading
+            ? <div className='center'>Loading...</div>
+            : (
+                <span className='center'>
+                  {data?.listBooks.error.message} {data?.listBooks.error.data.reason} 
+                </span>
+              )
+          }
+          </>
+        }     
+        
+      {!loading &&
+        <div className='buttons'>
+          <button 
+            disabled={page === 0 && true}
+            style={{backgroundColor: `${page === 0 ? 'gray' : '#6796ec'}`}}
+            onClick={() => {setPage(page - 1)}}>Prev</button>
+
+          <button
+            disabled={!data?.listBooks.meta?.hasMoreItems && true}
+            style={{backgroundColor: `${data?.listBooks.meta?.hasMoreItems ? '#6796ec' : 'gray'}`}}
+            onClick={() => {setPage(page + 1)}}>Next</button>
         </div>
+      }
     </div>
   );
 }
